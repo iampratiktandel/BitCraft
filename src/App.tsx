6cbfbbf58e007eb252a487bbbf18943f88a94fcd
Pratik Tandel <pratik.tandel@1rivet.comgit>
2024-07-04 13:14:44 +0530

    setup flowbite-react

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
+import { Button } from 'flowbite-react'
 import './App.css'
 
 function App() {
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <Button>Click me</Button>
+    </div>
   )
 }
 
